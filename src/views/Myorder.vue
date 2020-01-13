4bc75bdeee8e59ddd1abdf83d6affffb23e7fee3
dingyan <18539521005@163.com>
2020-01-13 16:18:40 +0800

    1.13 16:18

--- src/views/Myorder.vue
+++ src/views/Myorder.vue
@@ -1,6 +1,8 @@
 <template>
-    <div class="">
-        我的订单
+    <div class="myorder">
+      <header>
+        <router-link to="/My" class="header-left"><span>〈</span></router-link><span class="header-right">我的订单</span>
+      </header>
     </div>
 </template>
 
@@ -16,4 +18,17 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
+.myorder {
+    height 100%
+    background #eee
+    header {
+        height 0.6rem
+        line-height 0.6rem
+        font-size 0.3rem
+        font-weight 600
+        .header-left {
+            margin 0 35% 0 5%
+        }
+    }
+}
 </style>
