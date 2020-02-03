d207c79d633bebc54ec619bd705354c0aeff677d
15036285835 <2908105895@qq.com>
2020-02-03 17:25:26 +0800

    2.03

--- src/main.js
+++ src/main.js
@@ -2,62 +2,20 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from '@/router'
 import store from './store'
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-import '@/assets/js/font-size.js'
 import '@/assets/css/reset.css'
-=======
 import '@/assets/js/font-size.js'
-import '@/assets/css/reset.css'
 import '@/assets/css/iconfont.css'
-import '@/assets/css/iconfont1.css'
-import axios from 'axios'
-
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
-
-Vue.use(ElementUI);
-
-Vue.prototype.$axios = axios
-
->>>>>>> dingyan
-
-import '@/assets/css/iconfont1.css'
-
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
-
-import axios from 'axios'
-=======
-import axios from 'axios'
-import '@/assets/js/font-size.js'
-import '@/assets/css/reset.css'
-import '@/assets/css/iconfont1.css'
-//引入UI框架
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
->>>>>>> zp
-Vue.prototype.$axios = axios
-
-Vue.use(ElementUI);
-=======
-//重置字体大小.回复默认
-import '@/assets/js/font-size.js'
-import '@/assets/css/reset.css'
-//引入字体文件
-import '@/assets/css/iconfont1.css'
->>>>>>> wangziwen
-
-=======
-import '@/assets/js/font-size.js'
-import '@/assets/css/reset.css'
 import '@/assets/css/iconfont1.css'
 import '../mock/mock.js'
 import axios from 'axios'
+
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
+Vue.use(ElementUI);
+
 Vue.prototype.$axios = axios
->>>>>>> fan
+
 Vue.config.productionTip = false
 
 new Vue({
--- vue.config.js
+++ vue.config.js
@@ -1,78 +1,50 @@
-<<<<<<< HEAD
 const path = require('path')
 const url = require('url')
-<<<<<<< HEAD
-<<<<<<< HEAD
-const goodsList = require("./mock/goodsList.json")
-=======
 const findList = require('./mock/findList.json')
 const seller = require("./mock/data.json")
->>>>>>> zp
-=======
 const sylb = require("./mock/sylb.json")
->>>>>>> fan
-
 function resolve(dir) {
     return path.join(__dirname, dir)
 }
-
-<<<<<<< HEAD
 module.exports = {
     devServer: {
         port: 8080,
         before(app) {
-<<<<<<< HEAD
-            app.get("/api/getgoodsList",(req,res)=>{
-                setTimeout(()=>{
-                    res.json(goodsList)
-                },500)
-            })
-        }
-=======
-            app.get("/api/getSeller",(req,res)=>{
-                setTimeout(()=>{
+            app.get("/api/getSeller", (req, res) => {
+                setTimeout(() => {
                     res.json(seller["goods"])
-                },500)
+                }, 500)
             })
         },
->>>>>>> zp
-=======
-module.exports ={
-    devServer:{
-        port: 8080,
-        before(app) {
-            app.get("/api/getSeller",(req,res)=>{
-                    
-                    res.json({sylb})
-              
-            })
+            devServer: {
+                port: 8080,
+                before(app) {
+                    app.get("/api/getSeller", (req, res) => {
+                        res.json({ sylb })
+                    })
+                }
+            },
+            chainWebpack(config) {
+                config.resolve.alias
+                    //用com这个别名代表./src/components的完整路径
+                    .set('com', resolve('./src/components'))
+                    .set('page', resolve('./src/views'))
+            },
+            devServer: {
+                port: 8080,
+                // 服务器代理 (反向代理)
+                proxy: {
+                    '/api': {
+                        // php开启的服务器
+                        target: 'http://localhost/api',
+                        //允许跨域
+                        changeOrigin: true,
+                        pathRewrite: {
+                            '^/api': ''
+                        }
+                    }
+                }
+            }
         }
-    },
-    chainWebpack(config) {
-        config.resolve.alias
-            //用com这个别名代表./src/components的完整路径
-            .set('com', resolve('./src/components'))
-            .set('page', resolve('./src/views'))
->>>>>>> fan
-    }
+    
 }
-=======
-module.exports = {
-    devServer: {
-      port: 8080,
-      // 服务器代理 (反向代理)
-      proxy: {
-        '/api': {
-          // php开启的服务器
-          target: 'http://localhost/api',
-          //允许跨域
-          changeOrigin: true,
-          pathRewrite: {
-            '^/api': ''
-          }
-        }
-      }
-    }
-  }
-  
->>>>>>> dingyan
