7d0e50c36b1822f8bd7e3463323e85759569f6c0
15036285835 <2908105895@qq.com>
2020-02-04 15:57:26 +0800

    0204

--- vue.config.js
+++ vue.config.js
@@ -9,42 +9,32 @@ function resolve(dir) {
 module.exports = {
     devServer: {
         port: 8080,
+        proxy: {
+            '/api': {
+                // php开启的服务器
+                target: 'http://localhost/api',
+                //允许跨域
+                changeOrigin: true,
+                pathRewrite: {
+                    '^/api': ''
+                }
+            }
+        },
         before(app) {
             app.get("/api/getSeller", (req, res) => {
                 setTimeout(() => {
                     res.json(seller["goods"])
                 }, 500)
-            })
+            }),
+                app.get("/api/getSeller", (req, res) => {
+                    res.json({ sylb })
+                })
         },
-            devServer: {
-                port: 8080,
-                before(app) {
-                    app.get("/api/getSeller", (req, res) => {
-                        res.json({ sylb })
-                    })
-                }
-            },
-            chainWebpack(config) {
-                config.resolve.alias
-                    //用com这个别名代表./src/components的完整路径
-                    .set('com', resolve('./src/components'))
-                    .set('page', resolve('./src/views'))
-            },
-            devServer: {
-                port: 8080,
-                // 服务器代理 (反向代理)
-                proxy: {
-                    '/api': {
-                        // php开启的服务器
-                        target: 'http://localhost/api',
-                        //允许跨域
-                        changeOrigin: true,
-                        pathRewrite: {
-                            '^/api': ''
-                        }
-                    }
-                }
-            }
+        chainWebpack(config) {
+            config.resolve.alias
+                //用com这个别名代表./src/components的完整路径
+                .set('com', resolve('./src/components'))
+                .set('page', resolve('./src/views'))
         }
-    
+    }
 }
